f85215e2151bab2a1ac1a1844992c98a5fd47fee
raibbl <raibbl1997@gmail.com>
2023-12-01 19:20:56 -0600

    Dynamically get verse and randomize it and ui updates

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,7 @@ dependencies {
     implementation("androidx.wear.compose:compose-foundation:1.2.1")
     implementation("androidx.activity:activity-compose:1.8.1")
     implementation("androidx.core:core-splashscreen:1.0.1")
+    implementation("androidx.compose.material3:material3:1.1.2")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
--- app/src/main/java/com/raibbl/ayabelquran/presentation/MainActivity.kt
+++ app/src/main/java/com/raibbl/ayabelquran/presentation/MainActivity.kt
@@ -7,6 +7,8 @@
 package com.raibbl.ayabelquran.presentation
 
 import android.annotation.SuppressLint
+import android.content.Context
+import android.icu.util.Calendar
 import android.media.AudioAttributes
 import android.media.MediaPlayer
 import android.os.Bundle
@@ -16,45 +18,41 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Devices
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import androidx.wear.compose.foundation.CurvedLayout
 import androidx.wear.compose.material.Button
 import androidx.wear.compose.material.Icon
 import androidx.wear.compose.material.MaterialTheme
 import androidx.wear.compose.material.Text
-import androidx.wear.compose.material.TimeText
 import com.android.volley.Request
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
-import com.raibbl.ayabelquran.R
 import com.raibbl.ayabelquran.presentation.theme.AyaBelQuranTheme
 import org.json.JSONObject
+import java.util.Random
 
 
 class MainActivity : ComponentActivity() {
@@ -63,6 +61,7 @@ class MainActivity : ComponentActivity() {
         super.onDestroy()
         releaseMediaPlayer()
     }
+
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
@@ -71,34 +70,22 @@ class MainActivity : ComponentActivity() {
 
         setTheme(android.R.style.Theme_DeviceDefault)
         val responseString = mutableStateOf("Loading...")
-// ...
-
-// Instantiate the RequestQueue.
-        val queue = Volley.newRequestQueue(this)
-        val url = "https://api.alquran.cloud/v1/ayah/262/editions/quran-uthmani,en.asad"
-
-// Request a string response from the provided URL.
-        val stringRequest = StringRequest(
-            Request.Method.GET, url,
-            { response ->
-                val obj = JSONObject(response)
-
-                responseString.value  = obj.getJSONArray("data").getJSONObject(0).getString("text")
-            },
-            { responseString.value  = "That didn't work!" })
-
-// Add the request to the RequestQueue.
-        queue.add(stringRequest)
+        val verseNumber = mutableIntStateOf(0)
+        fetchVerseData(this, responseString, verseNumber, false)
         setContent {
-            WearApp(responseString.value,"https://cdn.islamic.network/quran/audio/128/ar.alafasy/262.mp3")
+            WearApp(
+                responseString.value,
+                "https://cdn.islamic.network/quran/audio/128/ar.alafasy/${verseNumber.intValue}.mp3",
+                onRefresh = {
+                    fetchVerseData(this, responseString, verseNumber, true)
+                })
         }
     }
 }
 
 
-
 @Composable
-fun WearApp(ayaText: String,ayaAudioUrl: String) {
+fun WearApp(ayaText: String, ayaAudioUrl: String, onRefresh: () -> Unit) {
     AyaBelQuranTheme {
         Box(
             modifier = Modifier
@@ -106,13 +93,13 @@ fun WearApp(ayaText: String,ayaAudioUrl: String) {
                 .background(MaterialTheme.colors.background),
             contentAlignment = Alignment.Center
         ) {
-            Aya(ayaText = ayaText, ayaAudioUrl =ayaAudioUrl )
+            Aya(ayaText = ayaText, ayaAudioUrl = ayaAudioUrl, onRefresh)
         }
     }
 }
 
 @Composable
-fun Aya(ayaText: String,ayaAudioUrl:String) {
+fun Aya(ayaText: String, ayaAudioUrl: String, onRefresh: () -> Unit) {
     Box(
         modifier = Modifier
             .fillMaxSize()
@@ -128,7 +115,7 @@ fun Aya(ayaText: String,ayaAudioUrl:String) {
                 textAlign = TextAlign.Center,
                 fontSize = 15.sp,
                 modifier = Modifier
-                    .padding(start=30.dp,end=30.dp,top=35.dp,bottom=40.dp)
+                    .padding(start = 30.dp, end = 30.dp, top = 35.dp, bottom = 40.dp)
                     .align(Alignment.CenterHorizontally),
                 color = MaterialTheme.colors.primary
             )
@@ -139,43 +126,39 @@ fun Aya(ayaText: String,ayaAudioUrl:String) {
             modifier = Modifier
                 .align(Alignment.BottomCenter)
                 .fillMaxWidth()
-                .height(35.dp),
+                .height(50.dp)
+                .padding(bottom = 20.dp),
             horizontalArrangement = Arrangement.Center
         ) {
-            PlayButton {
-                playAudioFromUrl(ayaAudioUrl)
+
+
+            IconButton(onClick = { playAudioFromUrl(ayaAudioUrl) }) {
+                Icon(
+                    imageVector = Icons.Filled.PlayArrow,
+                    contentDescription = "Play",
+                    modifier = Modifier.size(40.dp),
+
+                    )
+            }
+            IconButton(onClick = onRefresh) {
+                Icon(
+                    imageVector = Icons.Filled.Refresh,
+                    contentDescription = "Refresh",
+                    modifier = Modifier.size(40.dp),
+//                tint = Color.White // Change icon color if needed
+                )
             }
         }
     }
 
 }
 
-@Composable
-fun PlayButton(onClick: () -> Unit) {
-    Button(
-        onClick = onClick,
-        modifier = Modifier.fillMaxWidth()
-    ) {
-        Row {
-            Icon(
-                imageVector = Icons.Filled.PlayArrow,
-                contentDescription = "Play",
-//                Playmodifier = Modifier.padding(end = 8.dp)
-            )
-            Text("ترتيل", fontSize = 12.sp, fontWeight = FontWeight.Bold)
-        }
-    }
-}
-
-
-
-
-
 
 var mediaPlayer: MediaPlayer? = null
 
 fun playAudioFromUrl(url: String) {
-    if (mediaPlayer == null) {
+    if (mediaPlayer == null || mediaPlayer?.isPlaying == false) {
+        mediaPlayer?.release() // Release any previous MediaPlayer
         mediaPlayer = MediaPlayer().apply {
             setAudioAttributes(
                 AudioAttributes.Builder()
@@ -184,15 +167,11 @@ fun playAudioFromUrl(url: String) {
                     .build()
             )
             setDataSource(url)
-            prepareAsync() // Might take long! Use prepare() for local files.
+            prepareAsync() // Prepare the MediaPlayer asynchronously
             setOnPreparedListener { start() }
         }
     } else {
-        if (mediaPlayer?.isPlaying == true) {
-            mediaPlayer?.pause()
-        } else {
-            mediaPlayer?.start()
-        }
+        mediaPlayer?.pause()
     }
 }
 
@@ -202,4 +181,55 @@ fun releaseMediaPlayer() {
     mediaPlayer = null
 }
 
+fun generateDailyVerseUrl(randomize: Boolean): String {
+    val verseNumber: Int = if (randomize) {
+        // Completely random verse number
+        Random().nextInt(6236) + 1
+    } else {
+        // Random verse number based on the current day and year
+        val calendar = Calendar.getInstance()
+        val dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH)
+        val year = calendar.get(Calendar.YEAR)
+        val seed = year * 365 + dayOfMonth
+        Random(seed.toLong()).nextInt(6236) + 1
+    }
 
+    // Construct the URL with the generated verse number
+    return "https://api.alquran.cloud/v1/ayah/$verseNumber/editions/quran-uthmani,en.asad"
+}
+
+fun fetchVerseData(
+    context: Context,
+    responseString: MutableState<String>,
+    verseNumber: MutableState<Int>,
+    randomize: Boolean
+) {
+    val queue = Volley.newRequestQueue(context)
+    val url = generateDailyVerseUrl(randomize)
+
+    val stringRequest = StringRequest(
+        Request.Method.GET, url,
+        { response ->
+            try {
+                val obj = JSONObject(response)
+                val verseText = obj.getJSONArray("data").getJSONObject(0).getString("text")
+                val verseNum = obj.getJSONArray("data").getJSONObject(0).getInt("number")
+
+                responseString.value = verseText
+                verseNumber.value = verseNum
+
+                // Debugging log
+                Log.d("fetchVerseData", "Verse number: $verseNum, Verse text: $verseText")
+            } catch (e: Exception) {
+                responseString.value = "Error parsing data!"
+                Log.e("fetchVerseData", "Error: ${e.message}")
+            }
+        },
+        {
+            responseString.value = "That didn't work!"
+            Log.e("fetchVerseData", "Request failed")
+        }
+    )
+
+    queue.add(stringRequest)
+}
