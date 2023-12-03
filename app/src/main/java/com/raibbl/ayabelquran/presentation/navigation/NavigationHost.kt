0a98df7347ae8190f7858ea161c7d6adf0af5b51
raibbl <raibbl1997@gmail.com>
2023-12-03 15:09:02 -0600

    remove uneeded annotaion

--- app/src/main/java/com/raibbl/ayabelquran/presentation/navigation/NavigationHost.kt
+++ app/src/main/java/com/raibbl/ayabelquran/presentation/navigation/NavigationHost.kt
@@ -4,13 +4,11 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import androidx.wear.compose.material.ExperimentalWearMaterialApi
 import com.raibbl.ayabelquran.presentation.pages.AyaPage
 import com.raibbl.ayabelquran.presentation.pages.TafsirPage
 import org.json.JSONObject
 
 
-@OptIn(ExperimentalWearMaterialApi::class)
 @Composable
 fun NavigationHost(
     ayaText: String,
