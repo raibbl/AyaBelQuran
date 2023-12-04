8db962c81f39f4287ba44cb4b2a665e3f0d46199
raibbl <raibbl1997@gmail.com>
2023-12-03 22:07:36 -0600

    Change Buttons UI and Add Rotary scroll support

--- app/src/main/java/com/raibbl/ayabelquran/presentation/pages/AyaPage.kt
+++ app/src/main/java/com/raibbl/ayabelquran/presentation/pages/AyaPage.kt
@@ -1,30 +1,40 @@
 package com.raibbl.ayabelquran.presentation.pages
 
+import androidx.compose.foundation.focusable
 import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.animateScrollBy
+import androidx.compose.foundation.gestures.scrollBy
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material.icons.filled.Refresh
-import androidx.compose.material3.IconButton
+import androidx.compose.material3.Button
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.input.rotary.onRotaryScrollEvent
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import androidx.wear.compose.foundation.ExperimentalWearFoundationApi
+import androidx.wear.compose.foundation.rememberActiveFocusRequester
 import androidx.wear.compose.material.ExperimentalWearMaterialApi
 import androidx.wear.compose.material.FractionalThreshold
 import androidx.wear.compose.material.Icon
@@ -34,9 +44,10 @@ import androidx.wear.compose.material.rememberSwipeableState
 import androidx.wear.compose.material.swipeable
 import com.raibbl.ayabelquran.presentation.navigation.Screen
 import com.raibbl.ayabelquran.presentation.components.AnimatedSwipeHint
+import kotlinx.coroutines.launch
 import utilities.MediaPlayer
 
-@OptIn(ExperimentalWearMaterialApi::class)
+@OptIn(ExperimentalWearMaterialApi::class, ExperimentalWearFoundationApi::class)
 @Composable
 fun AyaPage(
     ayaText: String,
@@ -44,10 +55,13 @@ fun AyaPage(
     navController: NavHostController
 ) {
     val swipeableState = rememberSwipeableState(initialValue = 0)
+    val horizontalScrollState = rememberScrollState()
     val anchors = mapOf(
         0f to 0,
         with(LocalDensity.current) { -400.dp.toPx() } to 1
     )
+    val focusRequester = rememberActiveFocusRequester()
+    val coroutineScope = rememberCoroutineScope()
     Box(
         modifier = Modifier
             .fillMaxSize()
@@ -64,51 +78,79 @@ fun AyaPage(
                 navController.navigate(Screen.tafsirPage.route)
             }
         }
+
         Column(
             modifier = Modifier
-                .fillMaxWidth()
+                .onRotaryScrollEvent {
+                    coroutineScope.launch {
+                        horizontalScrollState.scrollBy(it.verticalScrollPixels)
+
+                        horizontalScrollState.animateScrollBy(0f)
+                    }
+                    true
+                }
+                .focusRequester(focusRequester)
+                .focusable()
                 .align(Alignment.TopCenter)
-                .verticalScroll(rememberScrollState())
+                .verticalScroll(horizontalScrollState)
         ) {
+
             Text(
                 text = ayaText,
                 textAlign = TextAlign.Center,
                 fontSize = 15.sp,
                 modifier = Modifier
-                    .padding(start = 30.dp, end = 30.dp, top = 35.dp, bottom = 40.dp)
+                    .padding(start = 30.dp, end = 30.dp, top = 35.dp, bottom = 10.dp)
                     .align(Alignment.CenterHorizontally),
                 color = MaterialTheme.colors.primary
             )
-        }
 
-        // Navigation Bar at the bottom
-        Row(
-            modifier = Modifier
-                .align(Alignment.BottomCenter)
-                .fillMaxWidth()
-                .height(50.dp)
-                .padding(bottom = 20.dp),
-            horizontalArrangement = Arrangement.Center
-        ) {
-
-
-            IconButton(onClick = { MediaPlayer.playAudioFromUrl() }) {
-                Icon(
-                    imageVector = Icons.Filled.PlayArrow,
-                    contentDescription = "Play",
-                    modifier = Modifier.size(40.dp),
-
-                    )
-            }
-            IconButton(onClick = onRefresh) {
-                Icon(
-                    imageVector = Icons.Filled.Refresh,
-                    contentDescription = "Refresh",
-                    modifier = Modifier.size(40.dp),
-//                tint = Color.White // Change icon color if needed
+            Button(
+                onClick = {
+                    navController.navigate(Screen.tafsirPage.route)
+                },
+                modifier = Modifier
+                    .padding(start = 30.dp, end = 30.dp, top = 5.dp, bottom = 10.dp)
+                    .align(Alignment.CenterHorizontally)
+            ) {
+                Text(
+                    text = "خمن السورة",
+                    textAlign = TextAlign.Center,
+                    fontSize = 12.sp
                 )
             }
+            // Navigation Bar at the bottom
+            Row(
+                modifier = Modifier
+                    .height(50.dp)
+                    .padding(bottom = 20.dp)
+                    .align(Alignment.CenterHorizontally),
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
+
+
+                Button(onClick = { MediaPlayer.playAudioFromUrl() }) {
+                    Icon(
+                        imageVector = Icons.Filled.PlayArrow,
+                        contentDescription = "Play",
+                        modifier = Modifier.size(25.dp),
+
+                        )
+                }
+                Spacer(modifier =  Modifier.width(5.dp) )
+                Button(onClick = onRefresh) {
+                    Icon(
+                        imageVector = Icons.Filled.Refresh,
+                        contentDescription = "Refresh",
+                        modifier = Modifier.size(25.dp),
+//                tint = Color.White // Change icon color if needed
+                    )
+                }
+            }
+
         }
+
+
     }
 
 }
--- app/src/main/java/com/raibbl/ayabelquran/presentation/pages/TafsirPage.kt
+++ app/src/main/java/com/raibbl/ayabelquran/presentation/pages/TafsirPage.kt
@@ -1,6 +1,9 @@
 package com.raibbl.ayabelquran.presentation.pages
 
+import androidx.compose.foundation.focusable
 import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.animateScrollBy
+import androidx.compose.foundation.gestures.scrollBy
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,14 +13,19 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.input.rotary.onRotaryScrollEvent
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import androidx.wear.compose.foundation.ExperimentalWearFoundationApi
+import androidx.wear.compose.foundation.rememberActiveFocusRequester
 import androidx.wear.compose.material.ExperimentalWearMaterialApi
 import androidx.wear.compose.material.FractionalThreshold
 import androidx.wear.compose.material.MaterialTheme
@@ -26,13 +34,17 @@ import androidx.wear.compose.material.rememberSwipeableState
 import androidx.wear.compose.material.swipeable
 import com.raibbl.ayabelquran.presentation.navigation.Screen
 import com.raibbl.ayabelquran.presentation.components.AnimatedSwipeHint
+import kotlinx.coroutines.launch
 import org.json.JSONObject
 import utilities.convertToArabicNumbers
 
-@OptIn(ExperimentalWearMaterialApi::class)
+@OptIn(ExperimentalWearMaterialApi::class, ExperimentalWearFoundationApi::class)
 @Composable
 fun TafsirPage(verseTafsir: JSONObject, navController: NavHostController) {
     val swipeableState = rememberSwipeableState(initialValue = 0)
+    val horizontalScrollState = rememberScrollState()
+    val focusRequester = rememberActiveFocusRequester()
+    val coroutineScope = rememberCoroutineScope()
     val anchors = mapOf(
         0f to 0,
         with(LocalDensity.current) { 400.dp.toPx() } to 1 // Swipe to the right
@@ -66,7 +78,18 @@ fun TafsirPage(verseTafsir: JSONObject, navController: NavHostController) {
             modifier = Modifier
                 .fillMaxWidth()
                 .align(Alignment.TopCenter)
-                .verticalScroll(rememberScrollState())
+                .onRotaryScrollEvent {
+                    coroutineScope.launch {
+                        horizontalScrollState.scrollBy(it.verticalScrollPixels)
+
+                        horizontalScrollState.animateScrollBy(0f)
+                    }
+                    true
+                }
+                .focusRequester(focusRequester)
+                .focusable()
+                .align(Alignment.TopCenter)
+                .verticalScroll(horizontalScrollState)
         ) {
             Text(
                 text = "${verseTafsir?.getJSONObject("surah")?.getString("name")} أية-${
