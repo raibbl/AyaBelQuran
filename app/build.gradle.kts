3447de58165c28225c90f29d676e9ee32801e701
raibbl <raibbl1997@gmail.com>
2024-09-30 01:51:33 -0500

    Upgrade gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
     defaultConfig {
         applicationId = "com.raibbl.ayabelquran"
         minSdk = 30
-        targetSdk = 34
+        targetSdk = 33
         versionCode = 1
         versionName = "1.0"
         vectorDrawables {
